6d648644ea38401d3c6b4e50b35dc85fa054de49
danilwhale <61111955+danilwhale@users.noreply.github.com>
2024-08-09 10:19:05 +0300

    prototype

---
+++ IkvmMc/Patches/ChunkRebuildPatch.cs
@@ -0,0 +1,37 @@
+﻿using System.Reflection;
+using System.Reflection.Emit;
+using com.mojang.rubydung.level;
+using HarmonyLib;
+
+namespace IkvmMc.Patches;
+
+[HarmonyPatch(typeof(Chunk), "rebuild")]
+public sealed class ChunkRebuildPatch
+{
+    private static Tile _magentaTile = (Tile)AccessTools.Constructor(typeof(Tile), [typeof(int)]).Invoke([3]);
+    private static FieldInfo _magentaTileField = AccessTools.Field(typeof(ChunkRebuildPatch), "_magentaTile");
+    private static FieldInfo _rockTileField = AccessTools.Field(typeof(Tile), "rock");
+    
+    [HarmonyTranspiler]
+    private static IEnumerable<CodeInstruction> RebuildTranspiler(IEnumerable<CodeInstruction> instructions)
+    {
+        var found = false;
+
+        foreach (var instruction in instructions)
+        {
+            if (instruction.opcode == OpCodes.Ldsfld && Equals(instruction.operand, _rockTileField))
+            {
+                yield return new CodeInstruction(OpCodes.Ldsfld, _magentaTileField);
+                found = true;
+                continue;
+            }
+
+            yield return instruction;
+        }
+
+        if (!found)
+        {
+            Console.Error.WriteLine("failed to find ldsfld with Tile::rock");
+        }
+    }
+}
---
+++ IkvmMc/Program.cs
@@ -0,0 +1,8 @@
+﻿using HarmonyLib;
+
+Harmony.DEBUG = true;
+var harmony = new Harmony("io.github.danilwhale.minecraft_ikvm");
+harmony.PatchAll();
+
+Console.WriteLine("s");
+com.mojang.rubydung.RubyDung.main(args);
